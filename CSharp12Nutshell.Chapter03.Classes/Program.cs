f0b6724745775d877500005308a14b61dadd6f8b
mabbaszadegan <mhmabbaszadegan@gmail.com>
2024-03-28 00:43:29 +0330

    deconstructors

---
+++ CSharp12Nutshell.Chapter03.Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp12Nutshell.Chapter03.Classes;
+
+Console.WriteLine("Hello, World!");
+
+
+#region Deconstructors
+
+var rect = new SampleDeconstructor(3, 4, "test");
+(float width, float height, string name) = rect;          // Deconstruction
+Console.WriteLine(width + " " + height + " " + name);    // 3 4 test
+
+var (w, h) = rect;          // Deconstruction
+Console.WriteLine(w + " " + h);    // 3 4
+
+rect.Deconstruct(out var n);        // Deconstruction
+Console.WriteLine(n);    // test
+
+#endregion
---
+++ CSharp12Nutshell.Chapter03.Classes/SampleDeconstructor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp12Nutshell.Chapter03.Classes
+{
+    internal class SampleDeconstructor
+    {
+        private readonly float Width, Height;
+
+        private readonly string Name;
+
+        public SampleDeconstructor(float width, float height, string name) =>
+            (Width, Height, Name) = (width, height, name);
+
+        public void Deconstruct(out float width, out float height, out string name)
+        {
+            width = Width;
+            height = Height;
+            name = Name;
+        }
+        public void Deconstruct(out float width, out float height)
+        {
+            width = Width;
+            height = Height;
+        }
+
+        public void Deconstruct(out string name)
+        {
+            name = Name;
+        }
+    }
+}
