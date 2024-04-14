59ed57e13f7ce622314691e48b62281659d49307
mabbaszadegan <mhmabbaszadegan@gmail.com>
2024-04-14 04:34:10 +0330

    chapter 4 - attributes

---
+++ CSharp12Nutshell.Chapter04.Attributes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+static void Foo(
+    [CallerMemberName] string memberName = null,
+    [CallerFilePath] string filePath = null,
+    [CallerLineNumber] int lineNumber = 0)
+{
+    Console.WriteLine(memberName);
+    Console.WriteLine(filePath);
+    Console.WriteLine(lineNumber);
+}
+
+Foo();
