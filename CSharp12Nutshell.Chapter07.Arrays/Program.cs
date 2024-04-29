df388a0efca07089b7a5f360c808096bead6c088
mabbaszadegan <mhmabbaszadegan@gmail.com>
2024-04-29 21:39:04 +0330

    chapter 7 - arrays

---
+++ CSharp12Nutshell.Chapter07.Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Construction and Indexing
+
+int[] myArray = { 1, 2, 3 };
+int first = myArray[0];
+int last = myArray[myArray.Length - 1];
+
+
+// Create a string array 2 elements in length:
+Array a = Array.CreateInstance(typeof(string), 2);
+a.SetValue("hi", 0);                             //  → a[0] = "hi";
+a.SetValue("there", 1);                          //  → a[1] = "there";
+string s = (string)a.GetValue(0);               //  → s = a[0];
+
+// We can also cast to a C# array as follows:
+string[] cSharpArray = (string[])a;
+string s2 = cSharpArray[0];
+
+object[] aSharpArray = (object[])a;
+
+Console.WriteLine(a.Rank);
+Demo();
+
+Console.WriteLine(a.Length);
+Array.Clear(a, 0, a.Length);
+Console.WriteLine(a.Length);
+
+void WriteFirstValue(Array a)
+{
+    Console.Write(a.Rank + "-dimensional; ");
+
+    // The indexers array will automatically initialize to all zeros, so
+    // passing it into GetValue or SetValue will get/set the zero-based
+    // (i.e., first) element in the array.
+
+    int[] indexers = new int[a.Rank];
+    Console.WriteLine("First value is " + a.GetValue(indexers));
+}
+
+void Demo()
+{
+    int[] oneD = { 1, 2, 3 };
+    int[,] twoD = { { 5, 6 }, { 8, 9 } };
+
+    WriteFirstValue(oneD);   // 1-dimensional; first value is 1
+    WriteFirstValue(twoD);   // 2-dimensional; first value is 5
+}
+#endregion
--- CSharp12Nutshell.Chapter07.Collections/Program.cs
+++ CSharp12Nutshell.Chapter07.Collections/Program.cs
@@ -35,7 +35,7 @@ int[] array = [1, 2];
 
 Array.Resize(ref array, 5);
 
-var a=Array.CreateInstance(typeof(int), 7);
+var a = Array.CreateInstance(typeof(int), 7);
 
 
 StringBuilder[] builders = new StringBuilder[5];
@@ -60,5 +60,93 @@ Console.WriteLine(se1.Equals(a2,
 
 StringBuilder[] builders2 = builders;
 StringBuilder[] shallowClone = (StringBuilder[])builders.Clone();
+
+#endregion
+
+#region Enumeration
+
+int[] myArray = { 1, 2, 3 };
+foreach (int val in myArray)
+    Console.WriteLine(val);
+
+Array.ForEach(myArray, (a) => Console.WriteLine(a));
+Array.ForEach(new[] { 1, 2, 3 }, Console.WriteLine);
+Array.ForEach([1, 2, 3], Console.WriteLine);
+
+#endregion
+
+#region Length and Rank
+
+string[,,] s3 = new string[5, 4, 8];
+Console.WriteLine($"{nameof(s3.Length)} -> {s3.Length}");                  //160
+Console.WriteLine($"{nameof(s3.LongLength)} -> {s3.LongLength}");          //160
+Console.WriteLine($"{nameof(s3.GetLength)} -> {s3.GetLength(1)}");         //4
+Console.WriteLine($"{nameof(s3.GetLongLength)} -> {s3.GetLongLength(2)}"); //8
+Console.WriteLine($"{nameof(s3.GetLowerBound)} -> {s3.GetLowerBound(2)}"); //0
+Console.WriteLine($"{nameof(s3.GetUpperBound)} -> {s3.GetUpperBound(2)}"); //7
+
+
+#endregion
+
+#region Searching
+
+Console.WriteLine(Array.BinarySearch([12, 5, 14, 7], 0, 2, 5));             // -1 because array is not sorted
+
+Console.WriteLine(Array.IndexOf(["a", "b", "5", "c", "d", "5"], "5"));      //2
+Console.WriteLine(Array.LastIndexOf(["a", "b", "5", "c", "d", "5"], "5"));  //5
+
+Console.WriteLine(string.Join(",", Array.FindAll(["a", "b", "5A", "c", "d", "5"], (a) => { return a == "5"; })));  //5,5
+Console.WriteLine(string.Join(",", Array.TrueForAll(["a", "b", "5A", "c", "d", "5"], (a) => { return a == "5"; })));  //5,5
+
+#endregion
+
+#region Sorting
+
+int[] arr = [2, 5, 8, 1];
+Array.Sort(arr);
+
+Console.WriteLine(string.Join(",", arr));  //1,2,5,8
+
+numbers = [3, 2, 1];
+string[] words = { "three", "two", "one" };
+Array.Sort(numbers, words);
+
+// numbers array is now { 1, 2, 3 }
+// words   array is now { "one", "two", "three" }
+
+#endregion
+
+#region Reversing Elements
+
+Array.Reverse(arr);
+Console.WriteLine(string.Join(",", arr));  //8,5,2,1
+
+#endregion
+
+#region Copying
+
+int[,] int3X3 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+int[] int3 = [1, 5, 6];
+var clone = int3X3.Clone();
+
+int[] copy3 = new int[3];
+int3.CopyTo(copy3, 0);
+
+int[,] copy3X3 = new int[3, 3];
+Array.Copy(int3X3, copy3X3, 5);
+
+Array.ConstrainedCopy(int3X3, 0, copy3X3, 3, 5);
+
+#endregion
+
+#region Converting and Resizing
+
+float[] reals = { 1.3f, 1.5f, 1.8f };
+int[] wholes = Array.ConvertAll(reals, r => Convert.ToInt32(r));
+
+// wholes array is { 1, 2, 2 }
+
+Array.Resize(ref wholes, 5);
+Console.WriteLine(string.Join(",", wholes));  // { 1, 2, 2, 0, 0 }
 
 #endregion
